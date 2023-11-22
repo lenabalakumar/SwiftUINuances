111ec819cf6ffe679ce21c45e2a44e6e2ee7d1a3
Lenabalakumar Subbarayan <lenabalakumar@gmail.com>
2023-11-22 14:34:58 +0000

    Initial Commit

---
+++ SwiftUINuances/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUINuances
+//
+//  Created by Lenabalakumar Subbarayan on 22/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUINuances/SwiftUINuancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINuancesApp.swift
+//  SwiftUINuances
+//
+//  Created by Lenabalakumar Subbarayan on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINuancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
